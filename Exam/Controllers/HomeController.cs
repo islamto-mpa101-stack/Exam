c60caf56f46b847c3877018f03823d97b7beaca1
javid-cff <javidrm-mpa101@code.edu.az>
2026-01-22 08:42:35 +0400

    Add project files.

---
+++ Exam/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+using System.Diagnostics;
+using Exam.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Exam.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+
+     
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        
+
+     
+    }
+}
